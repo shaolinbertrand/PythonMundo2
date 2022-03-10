c7a6bc61664f992b99d884f334dbdbac710e52c3
JEAN BERTRAND PAIXAO DA SILVA <jean_bertrand93@hotmail.com>
2022-03-10 12:03:35 -0400

    atualizando exercicios

--- estrutura while/exercicio58.py
+++ estrutura while/exercicio58.py
@@ -2,9 +2,8 @@ from random import random
 
 
 from random import *
-n = randint(1,10)
+n = randint(0,10)
 p = int(input('Adivinhe o numero que estou pensando: '))
-print(n)
 while(n!=p):
     p = int(input('Errou tente novamente: '))
 print('Acertou')
---
+++ estrutura while/exercicio59.py
@@ -0,0 +1,20 @@
+x = int(input('Digite o primeiro valor: '))
+y = int(input('Digite o segundo valor: '))
+o = 0
+while(o != 5):
+    print('Escolha a opção desejada')
+    print('[1] Somar')
+    print('[2] Multiplicar')
+    print('[3] Maior')
+    print('[4] Novos Numeros')
+    print('[5] Sair do Programa')
+    o = int(input("Opção: "))
+    if (o==1):
+        print('{} + {} = {}'.format(x,y,x+y))
+    elif(o==2):
+        print('{} x {} = {}'.format(x,y,x*y))
+    elif(o==3):
+        if x>y:
+            print('{} > {}'.format(x,y))
+        else:
+            print('{} > {}'.format(y,x))
