0fd2b74e97382a43065f8260d695375e6301f910
JEAN BERTRAND PAIXAO DA SILVA <jean_bertrand93@hotmail.com>
2022-03-11 08:35:46 -0400

    Create aula15.py

---
+++ estrutura while/aula15.py
@@ -0,0 +1,12 @@
+'''n = s = 0
+while True:
+    n=int(input('Digite um numero: '))
+    if n == 999:
+        break
+    s += n
+#print('a soma vale ',s)'''
+print(f'A soma vale {s}')
+nome = 'Jean'
+idade = 28
+salario = 1500
+print(f'Testando a string {nome:~^20} tem {idade} anos e ganha R${salario:.2f}')
